342da5b3574e611a46dc50e86cd34ceee2ff2048
GlennNieblasHernandez <36289429+GlennNieblasHernandez@users.noreply.github.com>
2021-08-13 13:59:35 -0500

    paquete dto/listinstructions}

---
+++ src/main/java/mx/santander/fiduciarioplus/dto/listinstructions/InstructionDto.java
@@ -0,0 +1,33 @@
+
+package mx.santander.fiduciarioplus.dto.listinstructions;
+
+import java.io.Serializable;
+
+import javax.annotation.Generated;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class InstructionDto implements Serializable{
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private BusinessDto business;
+	private SubBusinessDto subBusiness;
+	private InstructionsFileDto file;
+	private InstructionsTypeInstructionDto typeInstruction;
+	private StatusDto status;
+	private String date;
+
+}
---
+++ src/main/java/mx/santander/fiduciarioplus/dto/listinstructions/SubBusinessDto.java
@@ -0,0 +1,30 @@
+
+package mx.santander.fiduciarioplus.dto.listinstructions;
+
+import java.io.Serializable;
+
+import javax.annotation.Generated;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class SubBusinessDto implements Serializable {
+	
+	
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String id;
+
+}
