1956fdfcc6410ed1dcd9ace4d5f719d4526037db
GlennNieblasHernandez <36289429+GlennNieblasHernandez@users.noreply.github.com>
2021-08-30 13:23:44 -0500

    Agregar en repositorio Instructions query buc, business y subBusiness

--- src/main/java/mx/santander/fiduciarioplus/repository/IInstructionRepository.java
+++ src/main/java/mx/santander/fiduciarioplus/repository/IInstructionRepository.java
@@ -18,31 +18,12 @@ public interface IInstructionRepository extends JpaRepository<Instruction, Long>
 
 	List<Instruction> findByBucAndIdBusinessAndIdSubBusiness(String buc, Integer business, Integer subBusiness);
 
-	/*SELECT BUC, ID_BUSINESS, ID_SUB_BUSINESS, STATUS_INSTRUCTION, COUNT(STATUS_INSTRUCTION) AS STATUS FROM INSTRUCTION WHERE BUC = '980714019292' OR (ID_BUSINESS = '1' OR ID_SUB_BUSINESS = '1' )  GROUP BY STATUS_INSTRUCTION, ID_BUSINESS, ID_SUB_BUSINESS*/
-	
-	
 	@Query( value = "SELECT * FROM INSTRUCTION u WHERE u.buc = ?1", nativeQuery = true) 
-	/*@Query(value = "SELECT U.BUC, U.ID_BUSINESS, U.ID_SUB_BUSINESS, U.STATUS_INSTRUCTION,"
-			+ " COUNT(U.STATUS_INSTRUCTION) FROM INSTRUCTION U WHERE U.BUC=:buc"
-			+ " GROUP BY U.STATUS_INSTRUCTION, U.ID_BUSINESS, U.ID_SUB_BUSINESS", nativeQuery=true)*/
 	List<Instruction> findByBuc(String buc);
 	
-	/*@Query(value = "SELECT U.BUC, U.ID_BUSINESS, U.ID_SUB_BUSINESS, U.STATUS_INSTRUCTION,"
-			+ " COUNT(U.STATUS_INSTRUCTION) FROM INSTRUCTION U WHERE U.BUC=:buc"
-			+ " GROUP BY U.STATUS_INSTRUCTION, U.ID_BUSINESS, U.ID_SUB_BUSINESS", nativeQuery=true)*/
-	//@Modifying
-	//@Transactional
-	//List<Instruction> findStatusByBucAndIdBusinessAndIdSubBusiness(String buc);
-	//@Query(value="SELECT BUC, ID_BUSINESS, ID_SUB_BUSINESS, STATUS_INSTRUCTION, COUNT(STATUS_INSTRUCTION) AS STATUS FROM INSTRUCTION WHERE BUC = '980714019292' GROUP BY STATUS_INSTRUCTION, ID_BUSINESS, ID_SUB_BUSINESS",nativeQuery = true)
-	//@Modifying
-	//@Transactional
-	//List<Instruction> findByBuc(@Param("buc")String buc);
-	
-	
-    
-	
-	
-	
+	@Query( value = "SELECT * FROM INSTRUCTION u WHERE u.buc = ?1 AND ID_BUSINESS = ?2 AND ID_SUB_BUSINESS = ?3", nativeQuery = true) 
+	List<Instruction> findStatusByBucAndIdBusinessAndIdSubBusiness(String buc, Integer business, Integer subBusiness);
+
 	
 	
 }
