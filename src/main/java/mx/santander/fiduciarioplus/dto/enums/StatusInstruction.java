ccbf5dbe0eaf9dec28e8ff7e10419222309cddd1
GlennNieblasHernandez <36289429+GlennNieblasHernandez@users.noreply.github.com>
2021-08-30 13:00:06 -0500

    Se modifico Enum StatusInstruction

--- src/main/java/mx/santander/fiduciarioplus/dto/enums/StatusInstruction.java
+++ src/main/java/mx/santander/fiduciarioplus/dto/enums/StatusInstruction.java
@@ -1,10 +1,17 @@
 package mx.santander.fiduciarioplus.dto.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum StatusInstruction {
 
-	SOLICITADA,
-	ENTREGADA,
-	PROCESO,
-	ATENDIDA,
-	RECHAZADA;
+	SOLICITADA("SO"),
+	ENTREGADA("EN"),
+	PROCESO("PR"),
+	ATENDIDA("AT"),
+	RECHAZADA("RE");
+	
+	private String id;
 }
